e2be5ca769454959a1a42869f5e0232fd927e701
sd8644 <sd8644@srmist.edu.in>
2026-03-26 13:34:19 +0530

    added printing hello app file

---
+++ UC1_HelloApp.java
@@ -0,0 +1,11 @@
+/**
+ * UC1: Display "Hello World"
+ * * @author Sandeep
+ * @version 2
+ */
+public class UC1_HelloApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
